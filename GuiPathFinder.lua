0ad7851f656436bc979c1f0425a15abc2af2dea1
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-28 22:39:59 +0200

    Create GuiPathFinder.lua

---
+++ GuiPathFinder.lua
@@ -0,0 +1,13 @@
+--[[
+Whatever your mouse cursor touches, it will show the path of it. You can press "C" on your keyboard 
+and it will copy it to the clipboard too or "F" if you don't want it to show the path anymore. 
+You also choose whether you want it to get the paths for coregui stuff as well.
+]]--
+
+getgenv().settings = {
+   ["IgnoreCoreGui"] = false,
+   ["ToggleKeyBind"] = "F",
+   ["ClipboardKeyBind"] = "C"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Poll0none/Tools/main/GuiPathFinder-loadstring.lua"))()
