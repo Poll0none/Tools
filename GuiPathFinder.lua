7a8c45a4f31a78c75082b4dde5ceb96e90e8d092
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-28 22:48:41 +0200

    Update GuiPathFinder.lua

--- GuiPathFinder.lua
+++ GuiPathFinder.lua
@@ -1,4 +1,5 @@
 --[[
+https://v3rmillion.net/showthread.php?tid=1149189
 Whatever your mouse cursor touches, it will show the path of it. You can press "C" on your keyboard 
 and it will copy it to the clipboard too or "F" if you don't want it to show the path anymore. 
 You also choose whether you want it to get the paths for coregui stuff as well.
